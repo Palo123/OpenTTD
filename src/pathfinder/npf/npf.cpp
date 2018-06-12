e2c2efd8582ae947cc8df9104c577a029bd876b3
Palo123 <palo_123@azet.sk>
2018-06-12 18:15:57 +0200

    more NPF code

--- src/pathfinder/npf/npf.cpp
+++ src/pathfinder/npf/npf.cpp
@@ -571,6 +571,18 @@ static int32 NPFFindSafeTile(const AyStar *as, const OpenListNode *current)
 
 static int32 NPFFindCoupleTrain(AyStar *as, OpenListNode *current)
 {
+	AyStarNode *node = &current->path.node;
+	TileIndex tile = node->tile;
+	Trackdir td = node->direction;
+	
+	TrackdirBits tdb = TrackdirToTrackdirBits(td);
+	if (!HasReservedTracks(tile, TrackdirBitsToTrackBits(tdb)) || !IsRailStationTile(tile)) return AYSTAR_DONE;
+	Train *t = GetTrainForReservation(tile, TrackdirToTrack(td));
+	if (t == NULL) return AYSTAR_DONE;
+	if (t->current_order.IsType(OT_WAIT_COUPLE)) {
+		//if (TrainFitStation(t) && CheckOrderLoad(t) && CheckOrderCargoType(t)) return AYSTAR_FOUND_END_NODE;
+		return AYSTAR_FOUND_END_NODE;
+	}
 	return AYSTAR_DONE;
 }
 
