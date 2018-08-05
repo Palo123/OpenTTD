d1d3c080eb368553012ac8ca698d83d31299253d
Palo123 <palo_123@azet.sk>
2018-08-05 10:44:48 +0200

    Feature: allow train to reverse after coupling

--- src/ground_vehicle.cpp
+++ src/ground_vehicle.cpp
@@ -59,9 +59,6 @@ void GroundVehicle<T, Type>::PowerChanged()
 	max_te *= GROUND_ACCELERATION; // Tractive effort in (tonnes * 1000 * 9.8 =) N.
 	max_te /= 256;  // Tractive effort is a [0-255] coefficient.
 	if (this->gcache.cached_power != total_power || this->gcache.cached_max_te != max_te) {
-		/* Stop the vehicle if it has no power. */
-		if (total_power == 0) this->vehstatus |= VS_STOPPED;
-
 		this->gcache.cached_power = total_power;
 		this->gcache.cached_max_te = max_te;
 		SetWindowDirty(WC_VEHICLE_DETAILS, this->index);
